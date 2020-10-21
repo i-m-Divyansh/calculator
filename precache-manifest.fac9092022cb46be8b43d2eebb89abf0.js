fef030caabd2346cdda24041600f3cefd099b8f5
i_m.Divyansh <Divyanshbasera36@gmail.com>
2020-10-21 15:38:19 +0530

    Updates

---
+++ precache-manifest.fac9092022cb46be8b43d2eebb89abf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7733bf78c22e2eedf7bea799ff9939",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "21a1aea52f7e443d89b6",
+    "url": "/calculator/static/css/main.adb23efa.chunk.css"
+  },
+  {
+    "revision": "ec121505cea0d7fa9590",
+    "url": "/calculator/static/js/2.b4f21d96.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/calculator/static/js/2.b4f21d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a1aea52f7e443d89b6",
+    "url": "/calculator/static/js/main.e22bbeba.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
